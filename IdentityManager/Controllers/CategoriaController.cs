20ff8ad5a894590872874b287653da8f3da1b5d6
MiriamEG <mestradagarcia19@gmail.com>
2022-07-09 14:00:55 -0500

    Agregando proyecto al Local Repository

---
+++ IdentityManager/Controllers/CategoriaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityManager.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityManager.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IdentityManager/Controllers/ProductoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityManager.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityManager.Controllers
+{
+    public class ProductoController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+
+           
+        }
+    }
+}
